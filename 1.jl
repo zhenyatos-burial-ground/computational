ef3c04fb190a7f9d742ba0a16393520db53cb0e5
zhenyatos <zhenyatos999@gmail.com>
2020-10-04 17:02:05 +0300

    Update code for problem 1
    -Refactored to more Julia-like code

--- 1.jl
+++ 1.jl
@@ -14,67 +14,39 @@ A = [interval(1-ε, 1+ε) interval(1-ε, 1+ε);
     interval(1.1-ε, 1.1+ε) interval(1-ε, 1+ε)]
 
 # Determinant estimation
-detA = A[1, 1] * A[2, 2] - A[1, 2] * A[2, 1]
-println("Determinant value: ", detA)
-println("Determinant estimation criteria: ", 0 ∈ detA)
+det_A = A[1, 1] * A[2, 2] - A[1, 2] * A[2, 1]
+println("Determinant value: ", det_A)
+println("Determinant estimation criteria: ", 0 ∈ det_A)
 
 println("\nProblem 2")
 
 # Matrix
 A = Array{Interval{Float64}, 2}(undef, n, n)
-for i = 1:n
-    for j = 1:n
-        if i == j
-            A[i, j] = 1
-        else
-            A[i, j] = interval(0.0, ε)
-        end
-    end
+for i = 1:n, j = 1:n
+    A[i, j] = i == j ? 1 : interval(0.0, ε)
 end
 
 # Beck 1
-M = Array{Float64, 2}(undef, n, n)
-R = Array{Float64, 2}(undef, n, n)
-for i = 1:n
-    for j = 1:n
-        M[i, j] = mid(A[i, j])
-        R[i, j] = radius(A[i, j])
-    end
-end
+M = [mid(A[i, j]) for i=1:n, j=1:n]
+R = [radius(A[i, j]) for i=1:n, j=1:n]
 
-M1 = inv(M)
-for i = 1:n
-    for j = 1:n
-        M1[i, j] = abs(M1[i, j])
-    end
-end
+M1 = [abs(inv(M)[i, j]) for i=1:n, j=1:n]
 F = M1 * R
 
 λ = eigvals(F)
 m = size(λ, 1)
 ρ = abs(λ[m])
-res = ""
-if ρ < 1
-    res = "false"
-else
-    res = "unknown"
-end
 println("P = ", ρ)
-println("Beck criteria: ", res)
+println("Beck criteria: ", ρ < 1 ? "false" : "unknown")
 
 # Beck 2
 F = R * M1
-diagMax = F[1, 1]
+diag_max = F[1, 1]
 for i = 2:n
-    if (diagMax < F[i, i])
-        global diagMax = F[i, i]
+    if (diag_max < F[i, i])
+        global diag_max = F[i, i]
     end
 end
 
-if (diagMax >= 1)
-    res = "true"
-else
-    res = "unknown"
-end
-println("M = ", diagMax)
-println("Diagonal criteria: ", res)
+println("M = ", diag_max)
+println("Diagonal criteria: ", diag_max >= 1 ? "true" : "unknown")
