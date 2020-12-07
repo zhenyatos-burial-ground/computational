85d58be26cd421e572feb4e316c8053adde3e433
zhenyatos <zhenyatos999@gmail.com>
2020-12-07 18:31:25 +0300

    Update code for problem 4

--- 4/main.py
+++ 4/main.py
@@ -8,3 +8,28 @@ sup_b = np.loadtxt("data/sup_b.txt", ndmin=2)
 [tolmax, argmax, envs, ccode] = tolsolvty(A, A, inf_b, sup_b)
 
 print("max Tol = ", tolmax)
+
+signs = opt.Bounds([-np.inf, -np.inf, -np.inf, 0, 0, 0],
+                   [+np.inf, +np.inf, +np.inf, +np.inf, +np.inf, +np.inf])
+
+diag_rad_b = np.diag(np.squeeze(np.asarray(0.5 * (sup_b - inf_b))))
+C = np.block([[-A, -diag_rad_b],
+              [A, -diag_rad_b]])
+
+mid_b = 0.5 * (inf_b + sup_b)
+r = np.concatenate([-mid_b, mid_b])
+r = np.squeeze(np.asarray(r))
+
+pos_sign = (0, None)
+no_sign = (None, None)
+methods_list = ['interior-point', 'simplex']
+np.set_printoptions(precision=3, suppress=True)
+for method in methods_list:
+    print("Method: ", method)
+    res = opt.linprog([0, 0, 0, 1, 1, 1],
+                      A_ub=C,
+                      b_ub=r,
+                      bounds=[no_sign, no_sign, no_sign,
+                              pos_sign, pos_sign, pos_sign],
+                      method=method)
+    print(res.x)
