7f20f767e237be8ca2357e4dafcfe7d4dad21c8f
zhenyatos <zhenyatos999@gmail.com>
2020-12-25 02:05:10 +0300

    Update code for problem 5 (ONE MORE TIME)

--- 5/task_2.jl
+++ 5/task_2.jl
@@ -40,7 +40,7 @@ end
 function heuristical_solver(A, 𝐛, n_attempts)
     a = fill(-Inf, 18)
     b = fill(+Inf, 18)
-    for k = 1:50
+    for k = 1:35
         𝐂, 𝐝 = get_good_system(A, 𝐛, n_attempts)
         x0 = init_point(𝐂, 𝐝)
         x, iter = sub_diff(𝐂, 𝐝, x0, 0.001)
@@ -66,10 +66,30 @@ function solution_checker(A, 𝐛, x)
     return n_correct
 end
 
+
+function plot_bounds(𝐱, s)
+    inf_values = []
+    sup_values = []
+    mid_values = []
+    index = collect(1:36)
+    for i in index
+        push!(inf_values, 𝐱[i].inf)
+        push!(sup_values, 𝐱[i].sup)
+        push!(mid_values, (𝐱[i].inf + 𝐱[i].sup) / 2)
+    end
+    plot(index, inf_values, label = "inf", dpi = 300)
+    plot!(index, sup_values, label = "sup")
+    plot!(index, s, label = "solution")
+    plot!(index, mid_values, label = "mid")
+    savefig("bounds_plot.png")
+end
+
+
 function task_2()
     ε = 0.3
     A = readdlm("data\\A.txt")
-    b = readdlm("data\\b.txt")
+    b = readdlm("data\\bnew.txt")
+    s = readdlm("data\\snew.txt")
     𝐛 = ModalInterval.(b .- ε, b .+ ε)
 
     U = A[1:128, 19:36]
@@ -82,4 +102,5 @@ function task_2()
         show(io, "text/plain", x)
     end;
     println("Solved: ", solution_checker(A, 𝐛, x)) # should print 256 if solution obtained
+    plot_bounds(x, s)
 end
