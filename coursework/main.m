e2ab6be0a2c29b42cf4744ba26bfaad4702204c9
zhenyatos <zhenyatos999@gmail.com>
2020-12-29 12:27:08 +0300

    Update code for courework

--- coursework/main.m
+++ coursework/main.m
@@ -32,4 +32,34 @@ for i = 1:n
     ni_values(i) = ni;
 end
 plot(tau_values, ni_values);
-legend('$\underline{A}_{77} = 6$', '$\underline{A}_{77} = 10$', '$\underline{A}_{77} = 12$', 'interpreter','latex');
+
+A(7, 7, 1) = 12.8;
+for i = 1:n
+    [x, ni] = subdiff(A, b, tau_values(i), false);
+    ni_values(i) = ni;
+end
+plot(tau_values, ni_values);
+legend('$\underline{A}_{77} = 6$', ...
+    '$\underline{A}_{77} = 10$', ...
+    '$\underline{A}_{77} = 12$', ...
+    '$\underline{A}_{77} = 12.8$', 'interpreter','latex');
+saveas(gcf,'1.png')
+hold off;
+
+t = 6:0.05:14;
+nr_fr = length(t);
+frames = moviein(nr_fr); 
+writerObj = VideoWriter('2.avi');
+writerObj.FrameRate = 2;
+open(writerObj);
+for i = 1 : nr_fr
+    A(7, 7, 1) = t(i);
+    for k = 1:n
+        [x, ni] = subdiff(A, b, tau_values(k), false);
+        ni_values(k) = ni;
+    end
+    plot(tau_values, ni_values);
+    title(sprintf('%f', t(i)), 'interpreter','latex');
+    writeVideo(writerObj, getframe(gcf));
+end
+close(writerObj);
